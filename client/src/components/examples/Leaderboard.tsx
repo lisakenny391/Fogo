25738cf76a2dc1b8f5c04314744ceb10f865cae1
lisakenny391 <lisakenny391@gmail.com>
2025-09-20 12:40:51 +0100

    Delete client directory

--- client/src/components/examples/Leaderboard.tsx
+++
@@ -1,9 +0,0 @@
-import { Leaderboard } from '../Leaderboard';
-
-export default function LeaderboardExample() {
-  return (
-    <div className="p-4 max-w-2xl mx-auto">
-      <Leaderboard />
-    </div>
-  );
-}
--- client/src/components/examples/RecentActivity.tsx
+++
@@ -1,9 +0,0 @@
-import { RecentActivity } from '../RecentActivity';
-
-export default function RecentActivityExample() {
-  return (
-    <div className="p-4 max-w-2xl mx-auto">
-      <RecentActivity />
-    </div>
-  );
-}
